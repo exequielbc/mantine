a6bb998664cb6117c76f5abf4a6fe06a36b4beb2
Exequiel Bahamonde <12929395+exequielbc@users.noreply.github.com>
2023-09-14 15:49:45 +1000

    Add stricter type checking for rem utils function

--- src/mantine-styles/src/theme/utils/rem/rem.ts
+++ src/mantine-styles/src/theme/utils/rem/rem.ts
@@ -1,17 +1,17 @@
-function createConverter(units: string) {
-  return (px: unknown) => {
+type PixelUnit = number | `${number}px`;
+
+function createConverter(units: 'rem' | 'em') {
+  return (px: PixelUnit) => {
     if (typeof px === 'number') {
       return `${px / 16}${units}`;
     }
 
-    if (typeof px === 'string') {
-      const replaced = px.replace('px', '');
-      if (!Number.isNaN(Number(replaced))) {
-        return `${Number(replaced) / 16}${units}`;
-      }
+    const replaced = px.replace('px', '');
+    if (!Number.isNaN(Number(replaced))) {
+      return `${Number(replaced) / 16}${units}`;
     }
 
-    return px as string;
+    return px;
   };
 }
 
